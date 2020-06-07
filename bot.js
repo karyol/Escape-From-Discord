eaa06cca1929c0aa13dedae8000cdfcb4faf085a
Baron_Drakula <karyol.kj@gmail.com>
2020-06-07 19:21:13 +0200

    discord.js
    przeniesienie bota z discord.io na discord.js

--- bot.js
+++ bot.js
@@ -1,63 +1,44 @@
-var Discord = require('discord.io');
-var logger = require('winston');
-var auth = require('./auth.json');
-// Configure logger settings
-logger.remove(logger.transports.Console);
-logger.add(new logger.transports.Console, {
-    colorize: true
+const Discord = require('discord.js');
+const auth = require('./auth.json');
+
+const bot = new Discord.Client();
+
+const helpMessage = new Discord.MessageEmbed()
+    .setColor('#d83ecc')
+    .setTitle('Help')
+    .setDescription('eft.help - show help \n eft.w %s - search for %s on eft wiki');
+
+bot.on('ready', () => {
+    console.log('Logged in!');
 });
-logger.level = 'debug';
-// Initialize Discord Bot
-var bot = new Discord.Client({
-   token: auth.token,
-   autorun: true
-});
-bot.on('ready', function (evt) {
-    logger.info('Connected');
-    logger.info('Logged in as: ');
-    logger.info(bot.username + ' - (' + bot.id + ')');
-});
-bot.on('message', function (user, userID, channelID, message, evt) {
-    if (message.substring(0, 4) == 'eft.') {
-        var args = message.substring(4).split(' ');
+
+bot.on('message', message => {
+    if(message.content.substr(0, 4) == 'eft.')
+    {
+        var args = message.content.substr(4).split(' ');
         var cmd = args[0];
 
         args = args.splice(1);
-        switch(cmd) {
+        switch(cmd)
+        {
+            case 'hello':
+                message.channel.send('Hi!');
+            break;
+
             case 'w':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'https://escapefromtarkov.gamepedia.com/index.php?search=' + args + '&title=Special%3ASearch&go=Go'
-                });
-            break;
-
-            case 'channelID':
-                bot.sendMessage({
-                    to: channelID,
-                    message: channelID
-                });
-            break;
-
-            case 'roles':
-                bot.sendMessage({
-                    to: channelID,
-                    message: roles
-                });
+                message.channel.send('https://escapefromtarkov.gamepedia.com/index.php?search=' + args + '&title=Special%3ASearch&go=Go');
+                message.author.send('tak');
             break;
 
             case 'help':
-                bot.sendMessage({
-                    to:channelID,
-                    message: '"eft.help" - show help \n"eft.w %s" - search for %s on eft wiki'
-                });
+                message.channel.send(helpMessage);
             break;
 
             default:
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'If you need help type "eft.help"'
-                });
+                message.channel.send('If you need help type "eft.help"');
             break;
-         }
-     }
+        }
+    }
 });
+
+bot.login(auth.token);
