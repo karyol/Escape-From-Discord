127715308b11225e36f8f99cc331c26d103f731d
Baron_Drakula <karyol.kj@gmail.com>
2020-06-08 13:25:37 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -26,13 +26,13 @@ bot.on('message', message => {
             break;
 
             case 'w':
-                for(var i = 0; i < args.length; i++)
+                /*for(var i = 0; i < args.length; i++)
                 {
                     if(args[i] == ',')
                     {
                         args = args.splice(i, 1, '_');
                     }
-                }
+                }*/
                 message.channel.send(args);
                 message.channel.send('https://escapefromtarkov.gamepedia.com/index.php?search=' + args + '&title=Special%3ASearch&go=Go');
             break;
