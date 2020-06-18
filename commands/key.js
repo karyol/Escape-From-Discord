a235e862a21449d44c3c449600b057ca253115c1
Baron_Drakula <karyol.kj@gmail.com>
2020-06-18 14:32:06 +0200

    Update key.js

--- commands/key.js
+++ commands/key.js
@@ -3,13 +3,17 @@ const Discord = require('discord.js');
 const keys = new Array();
 const keyMessage = new Discord.MessageEmbed()
     .setColor('#f34336')
-    .setFooter('Market price can not be 100% accurate.');
+    .setFooter('Market price may not be 100% accurate.');
 
 keys.push(
-    {id: 'rb-bk key', name: 'RB-BK key', merchantPrice: '75,000₽', marketPrice: '75,000₽'},
-    {id: 'west wing room 205 key', name: 'West wing room 205 key', merchantPrice: '37,125₽', marketPrice: '675,000₽'},
-    {id: 'east wing room 205 key', name: 'East wing room 205 key', merchantPrice: '138,750₽', marketPrice: '150,000₽'}
+    {id: 'rb-bk key', name: 'RB-BK key', merchantPrice: '75,000', marketPrice: '75,000', map: 'Reserve'},
+    {id: 'rb-vo key', name: 'RB-VO key', merchantPrice: '75,000', marketPrice: '79,500', map: 'Reserve'},
+    {id: 'rb-ak key', name: 'RB-AK key', merchantPrice: '54,750', marketPrice: '200,000', map: 'Reserve'},
+    {id: 'west wing room 205 key', name: 'West wing room 205 key', merchantPrice: '37,125', marketPrice: '675,000', map: 'Shoreline'},
+    {id: 'east wing room 205 key', name: 'East wing room 205 key', merchantPrice: '138,750', marketPrice: '150,000', map: 'Shoreline'}
 );
+
+//{id: '', name: '', merchantPrice: '', marketPrice: '', map: ''},
 
 exports.run = (bot, message, args) => {
     var temp, txt;
@@ -25,7 +29,10 @@ exports.run = (bot, message, args) => {
                 if(temp == args.length)
                 {
                     keyMessage.setTitle(keys[i].name);
-                    keyMessage.setDescription('Therapist price: ' + keys[i].merchantPrice + '\nFlea Market price: ' + keys[i].marketPrice);
+                    keyMessage.setDescription(
+                        'Therapist price: ' + keys[i].merchantPrice + '₽' + 
+                        '\nFlea Market price: ' + keys[i].marketPrice + '₽' + 
+                        '\nMap: ' + keys[i].map);
                     message.channel.send(keyMessage);
                 }
             }
