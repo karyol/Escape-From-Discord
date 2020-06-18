c95e2c30cb30c8966da3057cd8de87eadebf9d7d
Baron_Drakula <karyol.kj@gmail.com>
2020-06-18 13:51:50 +0200

    key.js

--- commands/help.js
+++ commands/help.js
@@ -3,8 +3,8 @@ const Discord = require('discord.js');
 const helpMessage = new Discord.MessageEmbed()
     .setColor('#d83ecc')
     .setTitle('Help')
-    .setDescription('eft.help - show help \n eft.w %s - search for %s on eft wiki');
-    
+    .setDescription('eft.help - show help \n eft.w %s - search for %s on eft wiki \n eft.key %s - information about %s key');
+
 // eslint-disable-next-line no-unused-vars
 exports.run = (bot, message, args) => {
     message.channel.send(helpMessage).catch(console.error);
--- commands/key.js
+++ commands/key.js
@@ -2,10 +2,13 @@ const Discord = require('discord.js');
 
 const keys = new Array();
 const keyMessage = new Discord.MessageEmbed()
-    .setColor('#f00');
+    .setColor('#ff0000')
+    .setFooter('Market price can not be 100% accurate.');
 
 keys.push(
-    {name: 'RB-BK', merchantPrice: '75,000₽', marketPrice: '75,000₽'}
+    {name: 'RB-BK', merchantPrice: '75,000₽', marketPrice: '75,000₽'},
+    {name: 'West wing room 205 key', merchantPrice: '37,125₽', marketPrice: '675,000₽'},
+    {name: 'East wing room 205 key', merchantPrice: '138,750₽', marketPrice: '150,000₽'}
 );
 
 exports.run = (bot, message, args) => {
