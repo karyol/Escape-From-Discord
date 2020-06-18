e17fccdb3016006b19a535760b318dc892efefd3
Baron_Drakula <karyol.kj@gmail.com>
2020-06-18 12:50:45 +0200

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,5 +1,7 @@
+const Discord = require('discord.js');
+
 exports.run = (bot, message, args) => {
-    const helpMessage = new bot.Discord.MessageEmbed()
+    const helpMessage = new Discord.MessageEmbed()
         .setColor('#d83ecc')
         .setTitle('Help')
         .setDescription('eft.help - show help \n eft.w %s - search for %s on eft wiki');
