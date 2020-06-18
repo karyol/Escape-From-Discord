977d991676a0856e5773c44d65c5f73a6b3d69de
Baron_Drakula <karyol.kj@gmail.com>
2020-06-19 00:01:08 +0200

    Update ammo.js

--- commands/ammo.js
+++ commands/ammo.js
@@ -1,3 +1,11 @@
+const Discord = require('discord.js');
+
+const weapons = new Array();
+const ammoMessage = new Discord.MessageEmbed()
+    .setColor('#ffffff');
+
+weapons['sks'] = '7,62';
+
 exports.run = (bot, message, args) => {
     
 };
