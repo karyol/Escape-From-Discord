7a5de264cf71b1a0cd3eefcdb786a8ade2219e72
Baron_Drakula <karyol.kj@gmail.com>
2020-06-19 12:11:17 +0200

    Update ammo.js

--- commands/ammo.js
+++ commands/ammo.js
@@ -1,13 +1,62 @@
 const Discord = require('discord.js');
 
 const weapons = new Array();
+const ammo = new Array();
+const c762x39 = new Array();
 const ammoMessage = new Discord.MessageEmbed()
     .setColor('#ffffff');
 
-weapons['sks'] = '7,62';
+c762x39.push(
+    {type: 'HP', dmg: '87', pen: '15'},
+    {type: 'US', dmg: '56', pen: '29'},
+    {type: 'T45M (Tracer)', dmg: '62', pen: '30'},
+    {type: 'PS', dmg: '57', pen: '32'},
+    {type: 'BP', dmg: '58', pen: '47'}
+);
+
+ammo['7,62x39mm'] = c762x39;
+
+weapons.push(
+    {id: 'sks', name: 'SKS', cal: '7,62x39mm'}
+);
 
 exports.run = (bot, message, args) => {
-    
+    var temp, txt, count = 0;
+    for(var i = 0; i < weapons.length; i++)
+    {
+        temp = 0;
+        var ammoTxt;
+        var tempArray = new Array();
+        for(var j = 0; j < args.length; j++)
+        {
+            txt = args[j].toLowerCase();
+            if(weapons[i].id.includes(txt))
+            {
+                temp++;
+                if(temp == args.length)
+                {
+                    ammoMessage.setTitle(weapons[i].name + ' - ' + weapons[i].cal);
+                    tempArray = ammo[weapons[i]];
+                    for(var k = 0; k < tempArray.length; k++)
+                    {
+                        ammoTxt += tempArray[k].type + ' - Damage: ' + tempArray[k].dmg + ' - Penetration: ' + tempArray[k].pen + '\n';
+                    }
+                    ammoMessage.setDescription(ammoTxt);
+                    message.channel.send(ammoMessage);
+                    count++;
+                }
+
+                if(count >= 5)
+                {
+                    return;
+                }
+            }
+            else
+            {
+                break;
+            }
+        }
+    }
 };
 
 exports.help = {
