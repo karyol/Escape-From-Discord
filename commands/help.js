4f8b8059cb9672cbad66f16c05ec97afaa431c77
Baron_Drakula <karyol.kj@gmail.com>
2020-06-19 12:52:12 +0200

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -3,7 +3,11 @@ const Discord = require('discord.js');
 const helpMessage = new Discord.MessageEmbed()
     .setColor('#d83ecc')
     .setTitle('Help')
-    .setDescription('eft.help - show help \n eft.w %s - search for %s on eft wiki \n eft.key %s - information about %s key');
+    .setDescription(
+        'eft.help - show help' + '\n' +
+        'eft.w % - search for % on eft wiki' + '\n' +
+        'eft.key % - information about % key' + '\n' +
+        'eft.ammo % - ammo for % weapon [WIP]');
 
 // eslint-disable-next-line no-unused-vars
 exports.run = (bot, message, args) => {
