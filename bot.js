f62078375d4801cd5a1eb32deb6c837d995ccdb7
Baron_Drakula <karyol.kj@gmail.com>
2020-06-25 19:31:04 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,32 +14,6 @@ const config = {
 
 module.exports = { config };
 
-// bot.on('ready', () => {
-//     console.log('Logged in!');
-// });
-
-// bot.on('message', message => {
-//     if(message.content.substr(0, config.prefix.length) == config.prefix)
-//     {
-//         if(message.author.bot) return;
-//         if(message.content.indexOf(config.prefix) !== 0) return;
-
-//         var args = message.content.substr(config.prefix.length).split(/ +/g);
-//         var cmd = args[0].toLowerCase();
-
-//         const command = bot.commands.get(cmd);
-//         if(!command) 
-//         {
-//             message.channel.send('If you need help type "eft.help"');
-//             return;
-//         }
-
-//         args = args.splice(1);
-
-//         command.run(bot, message, args);
-//     }
-// });
-
 fs.readdir('./events', async (err, files) => {
     if(err) return console.error;
     files.forEach(file => {
