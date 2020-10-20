c060fb588a44f80e9c7adeeca5458adc359a5b75
Baron_Drakula <karyol.kj@gmail.com>
2020-10-20 12:31:24 +0200

    Update keys.js

--- commands/keys.js
+++ commands/keys.js
@@ -164,7 +164,7 @@ keys.push(
 //{id: '', name: '', tier: '', worth: '', map: ''},
 
 exports.run = (bot, message, args) => {
-    var temp, txt, tempName, tempTier, tempWorth;
+    var temp, txt, tempName = '', tempTier = '', tempWorth = '';
     for(var i = 0; i < maps.length; i++)
     {
         temp = 0;
